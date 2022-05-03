c92e64d5158408df2812d638122d765a22ae4123
hayaba <Hayabara995@gmail.com>
2022-05-03 11:56:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -17,3 +17,34 @@ function save() {
   console.log(count)
 }
 
+
+// ----------------------------- Practice part 1 --------------------------------
+let firstName = "Haya";
+let lastName = "Barakat";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+let name = "Linda";
+let greeting = "Hi there";
+function myGreeting() {
+  console.log(greeting + ", " + name + "!")
+}
+myGreeting()
+
+let myPoints = 3;
+
+function add3Points() {
+  myPoints += 3;
+}
+
+function remove1Point() {
+  myPoints --
+}
+add3Points()
+add3Points()
+add3Points()
+remove1Point()
+remove1Point()
+
+console.log(myPoints)
